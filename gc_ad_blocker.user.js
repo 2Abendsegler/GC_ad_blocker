175595731e9dba0280a895572df136a21623a659
2Abendsegler <2Abendsegler@users.noreply.github.com>
2017-11-15 09:48:52 +0100

    Add files via upload

---
+++ gc_ad_blocker.user.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name           GC ad blocker
+// @namespace      2Abendsegler
+// @version        0.1
+// @description    Advertising blocker on www.geocaching.com
+// @include        http*://www.geocaching.com/*
+// @include        http*://labs.geocaching.com/*
+// @require        http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js
+// @copyright      2Abendsegler
+// @author         2Abendsegler
+// @license        GNU General Public License v3.0
+// ==/UserScript==
+
+try {
+    $('#ctl00_uxBanManWidget').children().remove();
+    $('#div-message-center-ad').remove();
+    $('#ctl00_ContentBody_divContentSide').children().remove();
+    $('#ctl00_ContentBody_uxBanManWidget').children().remove();
+    $('#Content aside:not(#DashboardSidebar .advertisment) not(#leaderboard-ad)').children().remove();
+    $('#Content aside#DashboardSidebar .advertisment').children().remove();
+    $('#Content aside #leaderboard-ad').remove();
+} catch (e) {gc_error("error", e);}
+
+function gc_error(modul, err) {
+    var txt = "ERROR - " + modul + " - " + document.location.href + ": " + err.message + "\nStacktrace:\n" + err.stack + (err.stacktrace ? ("\n" + err.stacktrace) : "");
+    if (typeof(console) != "undefined") console.error(txt);
+}
