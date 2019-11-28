d6d2196debfd0b05ad58e7a3dd730ce951c99227
2Abendsegler <2Abendsegler@users.noreply.github.com>
2019-11-28 20:28:46 +0100

    Integrate new lists pages

--- gc_ad_blocker.user.js
+++ gc_ad_blocker.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           GC ad blocker
 // @namespace      2Abendsegler
-// @version        0.5
+// @version        0.4
 // @description    Advertising blocker on www.geocaching.com
 // @include        http*://www.geocaching.com/*
 // @include        http*://labs.geocaching.com/*
@@ -18,42 +18,66 @@ try {
     $('#div-message-center-ad').remove();
     $('#ctl00_ContentBody_divContentSide').children().remove();
     $('#ctl00_ContentBody_uxBanManWidget').children().remove();
+
     // account/dashboard, play/souvenircampaign/hiddencreatures
     $('#Content aside .advertisement').remove();
+
     // play/friendleague
     $('#Content aside #leaderboard-ad').remove();
+
     // account/lists, account/drafts
     $('#Content aside.sidebar-ad #lists-hub-ad').remove();
     $('#Content aside.sidebar-ad #draft-hub-ad').remove();
     $('#Content aside.sidebar-ad .contact').remove();
+
     // plan/lists
     function lists(waitCount) {
-        if ($('.structure aside.sidebar')[0]) $('.structure aside.sidebar').remove();
-        else {waitCount++; if (waitCount <= 100) setTimeout(function(){lists(waitCount);}, 100);}
+        if ($('aside.sidebar')[0]) {
+            var style = document.createElement('style');
+            style.innerHTML = 'aside.sidebar {width: 160px; height: 600px;}';
+            style.type = 'text/css';
+            $('head')[0].appendChild(style);
+            $('aside.sidebar').children().remove();
+        } else {waitCount++; if (waitCount <= 100) setTimeout(function(){lists(waitCount);}, 100);}
     }
     function buildObserverBody() {
-        var observerBody = new MutationObserver(function(mutations) { mutations.forEach(function(mutation) { lists(0); }); });
+        var observerBody = new MutationObserver(function(mutations) {
+            mutations.forEach(function(mutation) {
+                lists(0);
+            });
+        });
         var target = document.querySelector('body');
         var config = { attributes: true, childList: true, characterData: true };
         observerBody.observe(target, config);
     }
     function checkForBuildObserverBody(waitCount) {
-        if ($('body')[0]) buildObserverBody();
-        else {waitCount++; if (waitCount <= 100) setTimeout(function(){checkForBuildObserverBody(waitCount);}, 100);}
+        if ($('body')[0]) {
+            if ($('.gclh_buildObserverBody')[0]) return;
+            $('body').addClass('gclh_buildObserverBody');
+            buildObserverBody();
+        } else {waitCount++; if (waitCount <= 200) setTimeout(function(){checkForBuildObserverBody(waitCount);}, 50);}
     }
     function buildObserverButtons() {
-        var observerButtons = new MutationObserver(function(mutations) { mutations.forEach(function(mutation) { lists(0); }); });
-        var target = document.querySelector('.gc-button-group');
-        var config = { childList: true };
+        var observerButtons = new MutationObserver(function(mutations) {
+            mutations.forEach(function(mutation) {
+                lists(0);
+                checkForBuildObserverBody(0);
+            });
+        });
+        var target = document.querySelector('.desktop-nav-display');
+        var config = { attributes: true, childList: true, characterData: true };
         observerButtons.observe(target, config);
     }
     function checkForBuildObserverButtons(waitCount) {
         if ($('.gc-button-group')[0]) buildObserverButtons();
-        else {waitCount++; if (waitCount <= 100) setTimeout(function(){checkForBuildObserverButtons(waitCount);}, 100);}
+        else {waitCount++; if (waitCount <= 200) setTimeout(function(){checkForBuildObserverButtons(waitCount);}, 50);}
     }
-    lists(0);
-    checkForBuildObserverButtons(0);
-    checkForBuildObserverBody(0);
+    if (document.location.href.match(/\/plan\/lists/)) {
+        lists(0);
+        checkForBuildObserverBody(0);
+        checkForBuildObserverButtons(0);
+    }
+
 } catch (e) {gc_error("error", e);}
 
 function gc_error(modul, err) {
